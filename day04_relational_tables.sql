321a9d3e450087649520b60ba13f3ba05617a749
“Huseyin <sen19huseyin@gmail.com>
2023-01-25 16:29:32 +0300

    25.01.2023

---
+++ day04_relational_tables.sql
@@ -0,0 +1,173 @@
+use sys;
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- id, isim ve irtibat fieldlarinin oldugu bir tedarik tablosu olusturun.
+-- id field'ini Primary Key yapin.
+create table tedarik
+(
+id int primary key,
+isim varchar(25),
+irtibat varchar(15)
+);
+-- tedarikci_id , urun_id , urun_isim , musteri_isim  fieldlari olan urun
+-- tablosu olusturun. Bu tablodaki tedarikci_id fieldini tedarik tablosunun
+-- PK 'si ile Foreign Key yapin.
+create table urun
+(
+tedarikci_id int,
+urun_id int,
+urun_isim varchar(10),
+ musteri_isim varchar(15),
+ constraint urun_fk foreign key(tedarikci_id)
+ references tedarik (id)
+ 
+ );
+ select * from urun;
+ INSERT INTO tedarik VALUES(100, 'IBM', 'Ali Can');
+INSERT INTO tedarik VALUES(101, 'APPLE', 'Merve Temiz');
+INSERT INTO tedarik VALUES(102, 'SAMSUNG', 'Kemal Can');
+INSERT INTO tedarik VALUES(103, 'LG', 'Ali Can');
+INSERT INTO urun VALUES(100, 1001,'Laptop', 'Suleyman');
+INSERT INTO urun VALUES(101, 1002,'iPad', 'Fatma');
+INSERT INTO urun VALUES(102, 1003,'TV', 'Ramazan');
+INSERT INTO urun VALUES(103, 1004,'Phone', 'Ali Can');
+SELECT * FROM tedarik;
+SELECT * FROM urun;
+
+-- 'LG' firmasinda calisan 'Ali Can'in ismini 'Veli Can' olarak degistiriniz.
+UPDATE tedarik
+SET irtibat='Veli Can'
+WHERE irtibat='Ali Can';
+
+update tedarik 
+set irtibat='Veli Can'
+where irtibat='Ali can';
+
+update tedarik
+set irtibat='veli can'
+where irtibat='ali can';
+
+SELECT * FROM tedarik;
+SELECT * FROM urun;
+-- Ali Can'in aldigi urunun ismini Apple olarak degistirin
+update urun
+set urun_isim='Apple'
+where musteri_isim='Ali Can';
+
+-- Irtibat'i Merve Temiz olan kaydin sirket ismini getirin
+SELECT isim 
+FROM tedarik
+WHERE irtibat='Merve Temiz';
+
+/*
+a) Urun tablosundan Ali Can'in aldigi urunun ismini, 
+tedarik tablosunda irtibat Merve Temiz olan 
+sirketin ismi ile degistirin. */
+
+-- ***********************
+update urun
+set urun_isim = (select isim
+                 from tedarik
+                 where irtibat='Merve Temiz')
+where musteri_isim = 'Ali Can';
+/*-------------------------------------------------------------------------
+b) TV satin alan musterinin ismini, 
+IBM'in irtibat'i ile degistirin.
+-------------------------------------------------------------------------*/
+update urun
+set musteri_isim=(select irtibat 
+                  from tedarik
+                  where isim='IBM')
+where urun_isim='TV';
+
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/*-------------------------------------------------------------------------
+1) Cocuklar tablosu olusturun.
+ Icinde id,isim,veli_isim ve grade field'lari olsun. 
+ Id field'i Primary Key olsun.
+ --------------------------------------------------------------------------*/
+ CREATE TABLE cocuklar(
+id INT,
+isim VARCHAR(20),
+veli_isim VARCHAR(10),
+grade DOUBLE,
+CONSTRAINT id_pk PRIMARY KEY (id)
+);
+/*-------------------------------------------------------------------------
+ 2)  Kayitlari tabloya ekleyin.
+ (123, 'Ali Can', 'Hasan',75), 
+ (124, 'Merve Gul', 'Ayse',85), 
+ (125, 'Kemal Yasa', 'Hasan',85),
+ (126, 'Rumeysa Aydin', 'Zeynep',85);
+ (127, 'Oguz Karaca', 'Tuncay',85);
+ (128, 'Resul Can', 'Tugay',85);
+ (129, 'Tugay Kala', 'Osman',45);
+ --------------------------------------------------------------------------*/
+INSERT INTO cocuklar VALUES(123, 'Ali Can', 'Hasan',75);
+INSERT INTO cocuklar VALUES(124, 'Merve Gul', 'Ayse',85);
+INSERT INTO cocuklar VALUES(125, 'Kemal Yasa', 'Hasan',85);
+INSERT INTO cocuklar VALUES(126, 'Rumeysa Aydin', 'Zeynep',85);
+INSERT INTO cocuklar VALUES(127, 'Oguz Karaca', 'Tuncay',85);
+INSERT INTO cocuklar VALUES(128, 'Resul Can', 'Tugay',85);
+INSERT INTO cocuklar VALUES(129, 'Tugay Kala', 'Osman',45);
+/*-------------------------------------------------------------------------
+3)puanlar tablosu olusturun. 
+ogrenci_id, ders_adi, yazili_notu field'lari olsun, 
+ogrenci_id field'i Foreign Key olsun 
+--------------------------------------------------------------------------*/
+CREATE TABLE puanlar(
+ogrenci_id INT,
+ders_adi VARCHAR(10),
+yazili_notu DOUBLE,
+CONSTRAINT puanlar_fk 
+FOREIGN KEY (ogrenci_id) 
+REFERENCES cocuklar (id)
+);
+/*-------------------------------------------------------------------------
+4) puanlar tablosuna kayitlari ekleyin
+ ('123','kimya',75), 
+ ('124','fizik',65),
+ ('125','tarih',90),
+ ('126','kimya',87),
+ ('127','tarih',69),
+ ('128','kimya',93),
+ ('129','fizik',25)
+--------------------------------------------------------------------------*/
+INSERT INTO puanlar VALUES ('123','kimya',75);
+INSERT INTO puanlar VALUES ('124','fizik',65);
+INSERT INTO puanlar VALUES ('125','tarih',90);
+INSERT INTO puanlar VALUES ('126','kimya',87);
+INSERT INTO puanlar VALUES ('127','tarih',69);
+INSERT INTO puanlar VALUES ('128','kimya',93);
+INSERT INTO puanlar VALUES ('129','fizik',25);
+
+select * from cocuklar;
+select * from puanlar;
+/*-------------------------------------------------------------------------
+5) Tum cocuklarin grade'lerini notlar tablosundaki  yazili notlarini ile update edin. 
+--------------------------------------------------------------------------*/
+update puanlar
+set yazili_notu
+where ;
+
+/*-------------------------------------------------------------------------
+5) Tum cocuklarin yazili notlarini puanlar tablosundaki grade ile update edin. 
+--------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------
+6) Ismi Kemal Yasa olan ogrencinin grade'ini puanlar tablosundaki 
+ogrenci id'si 128 olan yazili notu ile update edin. 
+--------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------
+7) Ders adi fizik olan kayitlarin yazili notunu Oguz Karaca'nin grade'i
+ile update edin. 
+--------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------
+8) Ali Can'in grade'ini, 124 ogrenci_id'li yazili_notu ile guncelleyin.
+--------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------
+9) Ders adi Kimya olan yazili notlarini Rumeysa Aydin'in 
+grade'i ile guncelleyin.
+--------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------
+10) Ders adi tarih olan yazili notlarini Resul Can'in 
+grade'i ile guncelleyin.
+--------------------------------------------------------------------------*/
