801318b8c43f25382229b4326354d69d58e6a407
“Huseyin <sen19huseyin@gmail.com>
2023-01-26 16:11:39 +0300

    26.01.2023

--- day04_relational_tables.sql
+++ day04_relational_tables.sql
@@ -16,13 +16,13 @@ create table urun
 tedarikci_id int,
 urun_id int,
 urun_isim varchar(10),
- musteri_isim varchar(15),
- constraint urun_fk foreign key(tedarikci_id)
- references tedarik (id)
+musteri_isim varchar(15),
+constraint urun_fk foreign key(tedarikci_id)
+references tedarik (id)
  
  );
  select * from urun;
- INSERT INTO tedarik VALUES(100, 'IBM', 'Ali Can');
+INSERT INTO tedarik VALUES(100, 'IBM', 'Ali Can');
 INSERT INTO tedarik VALUES(101, 'APPLE', 'Merve Temiz');
 INSERT INTO tedarik VALUES(102, 'SAMSUNG', 'Kemal Can');
 INSERT INTO tedarik VALUES(103, 'LG', 'Ali Can');
@@ -145,29 +145,95 @@ select * from puanlar;
 /*-------------------------------------------------------------------------
 5) Tum cocuklarin grade'lerini notlar tablosundaki  yazili notlarini ile update edin. 
 --------------------------------------------------------------------------*/
-update puanlar
-set yazili_notu
-where ;
+update cocuklar
+set grade=(select yazili_notu
+		   from puanlar
+           where cocuklar.id=puanlar.ogrenci_id)
+where id;
 
-/*-------------------------------------------------------------------------
-5) Tum cocuklarin yazili notlarini puanlar tablosundaki grade ile update edin. 
---------------------------------------------------------------------------*/
 /*-------------------------------------------------------------------------
 6) Ismi Kemal Yasa olan ogrencinin grade'ini puanlar tablosundaki 
 ogrenci id'si 128 olan yazili notu ile update edin. 
 --------------------------------------------------------------------------*/
+update cocuklar
+set grade=(select yazili_notu
+		   from puanlar
+           where ogrenci_id=128)
+where isim='kemal yasa';
 /*-------------------------------------------------------------------------
 7) Ders adi fizik olan kayitlarin yazili notunu Oguz Karaca'nin grade'i
 ile update edin. 
 --------------------------------------------------------------------------*/
+update puanlar
+set yazili_notu=(select grade
+				from cocuklar
+                where isim='oguz karaca')
+where ders_adi='fizik';
 /*-------------------------------------------------------------------------
 8) Ali Can'in grade'ini, 124 ogrenci_id'li yazili_notu ile guncelleyin.
 --------------------------------------------------------------------------*/
+update cocuklar
+set grade=(select yazili_notu
+		   from puanlar
+           where ogrenci_id=124)
+where isim='Ali Can';
+
+select * from cocuklar;
+select * from puanlar;
 /*-------------------------------------------------------------------------
 9) Ders adi Kimya olan yazili notlarini Rumeysa Aydin'in 
 grade'i ile guncelleyin.
 --------------------------------------------------------------------------*/
-/*-------------------------------------------------------------------------
+ update puanlar
+ set yazili_notu=(select grade
+				 from cocuklar
+                 where isim='rumeysa aydin')
+ where ders_adi='kimya';
+ /*-------------------------------------------------------------------------
 10) Ders adi tarih olan yazili notlarini Resul Can'in 
 grade'i ile guncelleyin.
 --------------------------------------------------------------------------*/
+update puanlar
+set yazili_notu=(select grade
+					from cocuklar
+                    where isim='resul Can')
+where ders_adi='tarih';
+
+/*------------------------------------------------------------------------
+Mart_satislar isimli bir tablo olusturun, 
+icinde urun_id, musteri_isim, urun_isim ve urun_fiyat field'lari olsun
+--------------------------------------------------------------------------*/
+CREATE TABLE mart_satislar
+(
+ urun_id int,
+ musteri_isim varchar(20),
+ urun_isim varchar(10),
+ urun_fiyat int
+);
+INSERT INTO mart_satislar VALUES (10, 'Ali', 'Honda',75000);
+INSERT INTO mart_satislar VALUES (10, 'Ayse', 'Honda',95200);
+INSERT INTO mart_satislar VALUES (20, 'Hasan', 'Toyota',107500);
+INSERT INTO mart_satislar VALUES (30, 'Mehmet', 'Ford', 112500);
+INSERT INTO mart_satislar VALUES (20, 'Ali', 'Toyota',88000);
+INSERT INTO mart_satislar VALUES (10, 'Hasan', 'Honda',150000);
+INSERT INTO mart_satislar VALUES (40, 'Ayse', 'Hyundai',140000);
+INSERT INTO mart_satislar VALUES (20, 'Hatice', 'Toyota',60000);
+select * from mart_satislar;
+-- 1) Ismi hatice olan musterinin urun_id'sini 30,urun_isim'ini Ford yapin 
+update mart_satislar
+set urun_id=30, urun_isim='ford'
+where musteri_isim='hatice';
+-- 2) Toyota marka araclara %10 indirim yapin 
+update mart_satislar
+set urun_fiyat=urun_fiyat*0.9
+where urun_isim='toyota';
+
+-- 3) Ismi Ali olanlarin urun_fiyatlarini %15 artirin 
+update mart_satislar
+set urun_fiyat=urun_fiyat*1.15
+where musteri_isim='ali';
+
+-- 4) Honda araclarin urun id'sini 50 yapin.
+update mart_satislar
+set urun_id=50
+where urun_isim='honda';
